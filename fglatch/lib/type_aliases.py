0e1351b3dbc3396c61a7c01d2a9891fa61c42347
Matt Stone <matt@fulcrumgenomics.com>
2025-07-19 08:01:25 -0400

    feat: init LatchClient

---
+++ fglatch/lib/type_aliases.py
@@ -0,0 +1,9 @@
+"""Type aliases used by package modules."""
+
+from typing import TypeAlias
+
+LatchUserApiToken: TypeAlias = str
+"""A Latch user API token."""
+
+LatchWorkspaceId: TypeAlias = str
+"""A Latch workspace ID, e.g. "1234"."""
